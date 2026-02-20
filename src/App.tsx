6e09c9fec4087bd1226ebb80d5e399dd42112adf
9juanjuan7 <9juanjuan7@gmail.com>
2026-02-19 23:56:48 -0700

    upadted

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import DJScreen from "./pages/DJScreen";
-import RequestScreen from "./pages/RequestScreen";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<DJScreen />} />
       <Route path="/dj" element={<DJScreen />} />
-      <Route path="/request" element={<RequestScreen />} />
+      <Route path="/request" element={<DJScreen />} />
     </Routes>
   );
 }
